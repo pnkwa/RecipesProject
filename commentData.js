b7952e10137bd84d756c7ecb254e3f53de100707
pnkwa <prapanit_pajeekum@cmu.ac.th>
2023-10-01 15:19:44 +0700

    update comment data

---
+++ commentData.js
@@ -0,0 +1,36 @@
+const comments = [
+    {
+      id: 1,
+      text: "Great job!",
+      user: "John",
+      timestamp: "2021-08-15T10:30:00Z",
+    },
+    {
+      id: 2,
+      text: "Nice work!",
+      user: "Emily",
+      timestamp: "2021-08-15T11:15:00Z",
+    },
+    {
+      id: 3,
+      text: "Well done!",
+      user: "Sarah",
+      timestamp: "2021-08-15T12:00:00Z",
+    },
+    
+    {
+      id: 4,
+      text: "Impressive!",
+      user: "Michael",
+      timestamp: "2021-08-15T13:45:00Z",
+    },
+    {
+      id: 5,
+      text: "Keep it up!",
+      user: "Jessica",
+      timestamp: "2021-08-15T14:30:00Z",
+    },
+  ];
+  
+  exports.comments = comments;
+  
